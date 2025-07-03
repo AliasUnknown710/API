94748f12e912bc09e5551963f70b728dd69de0da
AliasUnknown710 <alexliss216@gmail.com>
2025-07-02 20:53:34 -0400

    Security Updates

--- config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Database configuration
-define('DB_HOST', 'localhost:3306');
-define('DB_USER', 'alexliss216_ISBA002');
-define('DB_PASSWORD', '&Q%Wzlj3ubT58lgn');
-define('DB_NAME', 'alexliss216_ISBA002');
