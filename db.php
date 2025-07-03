e2f081d403cd1bb0c78cebcb850989ea94f19f70
AliasUnknown710 <alexliss216@gmail.com>
2025-07-02 20:25:28 -0400

    DB configuration

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost:3306');
+define('DB_USER', 'alexliss216_ISBA002');
+define('DB_PASSWORD', '&Q%Wzlj3ubT58lgn');
+define('DB_NAME', 'alexliss216_ISBA002');
